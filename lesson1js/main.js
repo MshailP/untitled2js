8fffd8b85bc2a744b3622fcba47ba3cce6bf4207
MshailP <mihail.prohorov.02@gmail.com>
2022-11-03 22:26:15 +0200

    add lesson1js

---
+++ lesson1js/main.js
@@ -0,0 +1,44 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+
+let hello = 'hello'
+console.log(hello);
+let num = 1;
+console.log(num);
+let bool1 = true;
+console.log(bool1);
+let bool2 = false;
+console.log(bool2);
+const PI = 3.14;
+console.log(PI);
+const str = 'owu';
+console.log(str);
+
+hello = 'hello world';
+console.log(hello)
+
+bool1 = false
+console.log(bool1);
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+let firstName = 'mihail'
+let middleName = 'yriuovich'
+let lastName = 'prohorov'
+let result = firstName + ' ' + middleName +' ' + lastName;
+console.log(result);
+
+
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+let a = 100;
+console.log(typeof a);
+let b = '100';
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
+
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
